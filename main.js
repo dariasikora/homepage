f56097ed1480a028d0e6b18166d5cf997ef2ccf8
dariasikora <soltysiak.daria@gmail.com>
2022-06-06 20:17:21 +0200

    format box-sizing DOM

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ const age = "31";
 
 
 console.log(`Hej! Mam na imię ${name} i mam ${age} lat.`);
+
+
+const blee = dokument.querySelector('.page-content__description--js')
+blee.innerHTML = ('Czy to się uda?!')
+console.log(blee)
