3600eb6ff945a4e6c01529a7367a02c1d1032991
dariasikora <soltysiak.daria@gmail.com>
2022-06-08 22:15:28 +0200

    hamburger menu JS

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ const age = "31";
 console.log(`Hej! Mam na imię ${name} i mam ${age} lat.`);
 
 
-const blee = dokument.querySelector('.page-content__description--js')
-blee.innerHTML = ('Czy to się uda?!')
-console.log(blee)
+
+
+const hamburger = document.querySelector('.hamburger--js');
+
+hamburger.addEventListener('click', () => {
+    const nav = document.querySelector('.navigation--js');
+    nav.classList.toggle('navigation--open');
+})
